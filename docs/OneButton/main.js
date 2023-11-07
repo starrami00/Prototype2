43f9134a51349396fc021b397dceffff12dd3416
starrami00 <starrami@ucsc.edu>
2023-11-07 13:49:53 -0800

    Updating Level Design and Game Over Condition

--- docs/OneButton/main.js
+++ docs/OneButton/main.js
@@ -1,8 +1,7 @@
 title = "One Button";
 
 description = `
-[Tap]
- Jump / Double jump / Descent
+[Tap] Multiple jumps
 `;
 
 characters = [
@@ -21,129 +20,109 @@ ll l l
 llllll
 ll  ll
   `,
-  `
-  lll
-ll l l
- llll
-  ll
- l  l
- l  l
-`,
-  `
-  lll
-ll l l
- llll
- l  l
-ll  ll
-`,
-  `
-ll
- ll
- ll l
-llllll
-
-
-`,
-  `
-
-    l
-llllll
- ll
- ll
-ll
-`,
 ];
 
 options = {
   viewSize: { x: 200, y: 100 },
   isPlayingBgm: true,
   isReplayEnabled: true,
-  isDrawingScoreFront: true,
-  seed: 3,
+  seed: 50,
+  theme: "pixel",
 };
 
-const floorHeight = 90;
-const maxJumpCount = 2;
-
-/**
- * @type {{
- * pos: Vector, vy: number, jumpCount: number, isOnFloor: boolean,
- * multiplier: number, shots: Vector[], nextShotTicks: number
- * }}
- */
+/** @type {{pos: Vector, vy: number, posHistory: Vector[], isJumping: boolean}} */
 let player;
-/** @type {{ pos: Vector, vx: number, isFlying: boolean }[]} */
-let enemies;
-let nextEnemyTicks;
-let nextWallTicks;
-let nextFlyingTicks;
-let floorX;
-let animTicks;
+/** @type {{pos: Vector, width: number}[]} */
+let floors;
+let nextFloorDist;
+// Set the fixed height for the floor.
+const fixedFloorHeight = 70; 
+// Set the minimum gap width between floors.
+const minGapWidth = 20; 
+// Set the maximum gap width between floors.
+const maxGapWidth = 40; 
 
 function update() {
   if (!ticks) {
-    player = {
-      pos: vec(20, 50),
-      vy: 0,
-      jumpCount: 9,
-      isOnFloor: false,
-      multiplier: 1,
-      shots: [],
-      nextShotTicks: 0,
-    };
-    nextWallTicks = rnd(300, 400);
-    nextFlyingTicks = rnd(200, 300);
-    floorX = 0;
-    animTicks = 0;
+    player = { pos: vec(64, fixedFloorHeight - 5), vy: 0, posHistory: [], isJumping: false }; 
+    floors = [];
+    nextFloorDist = 0;
+    // Create an initial floor to start on
+    floors.push({
+      pos: vec(100, fixedFloorHeight), 
+      width: 90,
+    });
   }
-  const df = sqrt(difficulty);
-  animTicks += df;
-  color("light_cyan");
-  rect(floorX, floorHeight, 210, 9);
-  rect(floorX + 230, floorHeight, 210, 9);
-  floorX -= df;
-  if (floorX < -209) {
-    floorX += 230;
-  }
-  if (!player.isOnFloor) {
-    player.vy += (input.isPressed ? 0.1 : 0.3) * df;
+
+  const scr = sqrt(difficulty);
+
+  if (player.isJumping) {
+    const pp = vec(player.pos);
+    player.vy += (input.isPressed ? 0.05 : 0.2) * difficulty;
     player.pos.y += player.vy;
-    if (player.pos.y > floorHeight) {
-      play("hit");
-      player.pos.y = floorHeight;
-      player.isOnFloor = true;
-      player.jumpCount = 0;
-      player.multiplier = 1;
-    }
-  }
-  if (input.isJustPressed) {
-    if (player.jumpCount === maxJumpCount) {
-      play("laser");
-      player.vy += 9 * sqrt(df);
-    } else if (player.jumpCount < maxJumpCount) {
+    const op = vec(player.pos).sub(pp).div(9);
+    color("white");
+    times(9, () => {
+      pp.add(op);
+      box(pp, 6);
+    });
+  } else {
+    if (input.isJustPressed) {
       play("jump");
-      player.vy = -3 * sqrt(df);
-      player.isOnFloor = false;
+      player.vy = -2 * sqrt(difficulty);
+      player.isJumping = true;
     }
-    player.jumpCount++;
   }
-  color("yellow");
-  char(
-    addWithCharCode("a", floor(animTicks / 15) % 2),
-    player.pos.x + 3,
-    player.pos.y - 3
-  );
-  nextWallTicks--;
-  nextFlyingTicks--;
-  if (nextWallTicks < 0) {
-    const vx = -rnd(1, 2) * df;
-    const c = rndi(3, 6);
-    nextWallTicks = rnd(100, 600) / difficulty;
+
+  color("black");
+  char(player.vy < 0 ? "b" : "a", player.pos);
+
+  nextFloorDist -= scr;
+
+  if (nextFloorDist < 0) {
+    const gapWidth = rnd(minGapWidth, maxGapWidth);
+    const floorWidth = rnd(40, 80);
+    floors.push({
+      pos: vec(200 + gapWidth / 2 + floorWidth / 2, fixedFloorHeight),
+      width: floorWidth,
+    });
+    nextFloorDist += gapWidth + floorWidth + rnd(10, 30);
   }
-  if (nextFlyingTicks < 0) {
-    const vx = -rnd(1, 2) * df;
-    const c = rndi(1, 5);
-    const p = vec(206, rnd(50, 80));
-    nextFlyingTicks = rnd(100, 400) / difficulty;
+
+  remove(floors, (f) => {
+    f.pos.x -= scr;
+    color("light_yellow");
+    const c = box(f.pos, f.width, 4).isColliding.rect;
+    if (player.vy > 0 && c.white) {
+      player.pos.y = f.pos.y - 5;
+      player.isJumping = false;
+      player.vy = 0;
+    }
+    return f.pos.x < -f.width / 2;
+  });
+
+  player.posHistory.forEach((p) => {
+    p.x -= scr;
+  });
+
+  player.posHistory.unshift(vec(player.pos));
+
+  if (player.posHistory.length > 99) {
+    player.posHistory.pop();
+  }
+
+  color("transparent");
+
+  if (!player.isJumping) {
+    if (!box(player.pos.x, player.pos.y + 4, 9, 2).isColliding.rect.light_yellow) {
+      player.isJumping = true;
+    }
+  }
+  color("black");
+  char(player.vy < 0 ? "b" : "a", player.pos);
+
+  if (player.pos.y > 99) {
+    play("explosion");
+    end();
   }
 }
